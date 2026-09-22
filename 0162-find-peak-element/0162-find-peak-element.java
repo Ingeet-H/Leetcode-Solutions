a3767c0d94d068149aeb56a4abd6fc8a2c6fb293
Ingeet Halder <ingeethalder7376@gmail.com>
2026-09-22 23:47:10 +0530

    Time: 0 ms (100%), Space: 44 MB (75.35%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int start = 0, end = arr.length-1,mid = 0;
+        while(start!=end){
+            mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }
+            if(arr[mid]<arr[mid+1]){
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
